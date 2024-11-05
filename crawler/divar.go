49aa7db0dd5af393242012a93002798f89ed9cf0
Radmehr <radmehr.private@gmail.com>
2024-11-05 11:01:22 +0330

    get all targets for divar

--- crawler/divar.go
+++ crawler/divar.go
@@ -3,9 +3,30 @@ package main
 import (
 	"fmt"
 	"log"
-	"math/rand"
-	"sync"
-	"time"
 
 	"github.com/gocolly/colly"
 )
+
+const url string = "https://divar.ir/s/iran/real-estate?page=%d"
+
+func getTargets(crawler colly.Collector, current_pg int) []*Advertisement {
+
+	var Ads []*Advertisement
+
+	crawler.OnHTML("a[href]", func(h *colly.HTMLElement) {
+		// Checks if the attribute class is same with the given class
+		// This class is for post link
+		if h.Attr("class") == "kt-post-card__action" {
+			link := h.Request.AbsoluteURL(h.Attr("href"))
+			Ads = append(Ads, &Advertisement{Link: link}) // Creates an Ad and adds a link to it
+		}
+	})
+
+	crawler.OnRequest(func(r *colly.Request) {
+		log.Println("GRABBED TARGETS FROM: ", r.URL) // logs the request url
+	})
+
+	crawler.Visit(fmt.Sprintf(url, current_pg)) // Starts sending request
+
+	return Ads
+}
