0be25fa6b078b4a24672e0e0cc6e9877de5f1ffc
lhiradi <hiradjalilian@gmail.com>
2024-11-12 22:24:36 +0330

    minor changes on csv

--- csv/csv.go
+++ csv/csv.go
@@ -8,24 +8,27 @@ import (
 	"os"
 	"reflect"
 
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
-func ExportCsv(telID string, db *gorm.DB) error {
+func ExportCsv(telID string, db *gorm.DB) (string, error) {
 	u := repository.NewGormUser(db)
 
 	// Get user by telegramID
 	user, err := u.GetByTelegramId(telID)
 	if err != nil {
 		log.Println("Couldn't find user: ", err)
-		return err
+		return "", err
 	}
 
+	fileName := fmt.Sprintf("%s.csv", uuid.New()) // Unique file name
 	// Create a CSV file
-	f, err := os.Create("../telegram/Ads.csv")
+	f, err := os.Create("../telegram/" + fileName)
+
 	if err != nil {
 		log.Println("Couldn't create csv file: ", err)
-		return err
+		return "", err
 	}
 	defer f.Close()
 
@@ -36,7 +39,7 @@ func ExportCsv(telID string, db *gorm.DB) error {
 	header := []string{"لینک", "طول جغرافیایی", "عرض جغرافیایی", "توضیحات", "قیمت فروش", "قیمت اجاره", "مقدار رهن", "شهر", "محله", "متراژ", "تعداد اتاق", "دسته بندی", "سن بنا", "دسته بندی آپارتمان/ویلا", "طبقه", "انباری", "آسانسور", "عنوان آگهی", "لینک عکس"}
 	if err := w.Write(header); err != nil {
 		log.Println("error writing header to csv:", err)
-		return err
+		return "", err
 	}
 
 	for _, v := range user.Ads {
@@ -83,8 +86,8 @@ func ExportCsv(telID string, db *gorm.DB) error {
 
 		if err := w.Write(record); err != nil {
 			log.Println("error writing record to csv:", err)
-			return err
+			return "", err
 		}
 	}
-	return nil
+	return fileName, nil
 }
