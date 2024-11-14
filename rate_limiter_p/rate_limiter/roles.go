6bb3aca257a9425375627510fa47303d2a5e3561
Ehsan Sobhani <52486321+alefsobhani@users.noreply.github.com>
2024-11-15 00:47:38 +0330

    Update roles.go

--- rate_limiter_p/rate_limiter/roles.go
+++ rate_limiter_p/rate_limiter/roles.go
@@ -1,11 +1,10 @@
-
 package rate_limiter
 
 func getLimitByRole(role string) int {
     switch role {
     case "admin":
         return 100
-    case "premium_user":
+    case "super_user":
         return 50
     default:
         return 20
